64e1c903d65976e0d1a2627f4215949777770b99
Ivan Barabanov <barabanov_vanechka06@mail.ru>
2026-03-24 16:38:31 +0400

    easy methods written

--- Set.cpp
+++ Set.cpp
@@ -2,3 +2,84 @@
 
 using namespace std;
 
+Set::Set() :_root(nullptr) {};
+
+
+Node* Set::tree_copy(Node* root) {
+	if (!root) {
+		return nullptr;
+	}
+
+	Node* new_node = new Node;
+	new_node->value = root->value;
+
+	if (root->left) {
+		new_node->left = tree_copy(root->left);
+	}
+	else {
+		new_node->left = nullptr;
+	}
+
+	if (root->right) {
+		new_node->right = tree_copy(root->right);
+	}
+	else {
+		new_node->right = nullptr;
+	}
+}
+
+Set::Set(const Set& copy) {
+	_root=tree_copy(copy._root)
+}
+
+
+void Set::tree_print(Node* root) {
+	if (!root) {
+		return;
+	}
+	tree_print(root->left);
+	cout << root->value << " ";
+	tree_print(root->right);
+}
+
+void Set::print()const {
+	tree_print(_root);
+}
+
+bool Set::tree_contains(Node* root, int key){
+	Node* ptr = root;
+	while (ptr) {
+		if (ptr->value == key) {
+			return true;
+		}
+		else if (ptr->left < key) {
+			return tree_contains(root->left, key);
+		}
+		else {
+			return tree_contains(root->right, key);
+		}
+	}
+	return false;
+}
+
+bool Set::contains(int key) const {
+	return tree_contains(_root, key);
+}
+
+size_t Set::tree_size(Node* root) {
+	if (!root) {
+		return 0;
+	}
+	return tree_size(root->left) + 1 + tree_size(root->right);
+}
+
+size_t Set::size() const {
+	return tree_size(_root);
+}
+
+int Set::tree_balance_index(Node* root){}
+
+bool Set::strictly_balanced()const {
+
+}
+
--- Set.h
+++ Set.h
@@ -1,26 +1,35 @@
 #ifndef SET_H
 #define SET_H
 
-struct SetNode {
-	int _value;
-	SetNode* _left;
-	SetNode* _right;
+struct Node {
+	int value = 0;
+	Node* left = nullptr;
+	Node* right = nullptr;
 };
 
 
 class Set {
 private:
-	SetNode* root;
+	Node* _root;
+
+	Node* tree_copy(Node* root);
+	void tree_print(Node* root);
+	bool tree_contains(Node* root, int key);
+	size_t tree_size(Node* root);
 public:
-	SetNode();
-	SetNode(const SetNode& copy);
+	Set();
+	Set(const Set& copy);
 
 
 	void print() const;
 	bool insert(int key);
 	bool contains(int key) const;
 	bool erase(int key);
+
+	size_t size() const;
 	bool strictly_balanced() const;
+
+
 };
 
 #endif
