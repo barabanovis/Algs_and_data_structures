a7c807473f9e8ef6dd309038ae5522e797584bdb
Ivan Barabanov <barabanov_vanechka06@mail.ru>
2026-03-29 22:03:39 +0400

    updated contains

--- Set.cpp
+++ Set.cpp
@@ -46,27 +46,33 @@ void Set::print()const {
 	tree_print(_root);
 }
 
-bool Set::tree_contains(Node* root, int key){
-	Node* ptr = root;
+
+
+bool Set::contains(int key) const {
+	Node* ptr = _root;
 	while (ptr) {
-		if (ptr->value == key) {
+		if (ptr->key == key) {
 			return true;
 		}
-		else if (ptr->left < key) {
-			return tree_contains(root->left, key);
+		else if (key < ptr->key) {
+			ptr = ptr->left;
 		}
 		else {
-			return tree_contains(root->right, key);
+			ptr = ptr->right;
 		}
 	}
 	return false;
 }
 
+<<<<<<< HEAD
+size_t Set::tree_size(Node* root) {
+=======
 bool Set::contains(int key) const {
 	return tree_contains(_root, key);
 }
 
 size_t Set::tree_size(Node* root) const {
+>>>>>>> 4ef7c47d41c889f3ac88e7eccf4ac007ddefb843
 	if (!root) {
 		return 0;
 	}
@@ -77,6 +83,25 @@ size_t Set::size() const {
 	return tree_size(_root);
 }
 
+<<<<<<< HEAD
+int Set::tree_balance_index(Node* root){}
+
+
+size_t max(size_t a, size_t b) {
+	if (a > b) {
+		return a;
+	}
+	return b;
+}
+
+size_t Set::tree_height(Node* root) {
+	if (!root) {
+		return 0;
+	}
+	else {
+		return max(tree_height(root->left), tree_height(root->right)) + 1;
+	}
+=======
 bool Set::tree_isbalanced(Node* root) const{
 	if (tree_isbalanced(root->left) && tree_isbalanced(root->right)) {
 		int ind = tree_size(root->left) - tree_size(root->right);
@@ -90,5 +115,6 @@ bool Set::tree_isbalanced(Node* root) const{
 
 bool Set::strictly_balanced()const {
 	return tree_isbalanced(_root);
+>>>>>>> 4ef7c47d41c889f3ac88e7eccf4ac007ddefb843
 }
 
