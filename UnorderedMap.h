53eef62db289a2362ebf3215d2bde6df6a94a3dc
Ivan Barabanov <barabanov_vanechka06@mail.ru>
2026-04-26 21:11:15 +0400

    add main methods

--- UnorderedMap.h
+++ UnorderedMap.h
@@ -10,6 +10,8 @@ struct Node {
 	Node(int key, T value, Node* next) :key(key), value(value), next(next) {};
 };
 
+
+
 template <typename T>
 class UnorderedMap {
 private:
@@ -17,7 +19,7 @@ private:
 	size_t _size;
 	size_t _capacity;
 
-	static int hash(const T& value);
+	static size_t hash(const T& value);
 public:
 	UnorderedMap(const size_t size);
 
@@ -43,7 +45,7 @@ public:
 };
 
 template<typename T>
-int UnorderedMap<T>::hash(const T& value) {
+size_t UnorderedMap<T>::hash(const T& value) {
 	return value % _capacity;
 }
 
@@ -79,12 +81,12 @@ UnorderedMap<T>::UnorderedMap(const UnorderedMap<T>& copy) {
 			continue;
 		}
 
-		_table[i] = new Node<T>(copy._table[i]->value, nullptr);
+		_table[i] = new Node<T>(copy._table[i]->key, copy._table[i]->value, nullptr);
 		Node<T>* ptr_table = _table[i];
 		Node<T>* ptr_copy = copy._table[i]->next;
 
 		while (ptr_copy) {
-			ptr_table->next = new Node<T>(ptr_copy->value,nullptr);
+			ptr_table->next = new Node<T>(ptr_copy->key, ptr_copy->value,nullptr);
 			ptr_copy = ptr_copy->next;
 		}
 	}
@@ -93,11 +95,9 @@ UnorderedMap<T>::UnorderedMap(const UnorderedMap<T>& copy) {
 
 template <typename T>
 UnorderedMap<T>& UnorderedMap<T>::operator=(const UnorderedMap<T>& copy) {
+	//Удаление текущих данных
 	for (size_t i = 0; i < _capacity; ++i) {
 		Node<T>* ptr = _table[i];
-		if (!ptr) {
-			continue;
-		}
 		while (ptr) {
 			Node<T>* ptr_next = ptr->next;
 			delete ptr;
@@ -106,6 +106,7 @@ UnorderedMap<T>& UnorderedMap<T>::operator=(const UnorderedMap<T>& copy) {
 		}
 	}
 	
+	// Запись новых
 	_size = copy._size;
 	_capacity = copy._capacity;
 	_table = new Node<T>*[_size];
@@ -116,7 +117,7 @@ UnorderedMap<T>& UnorderedMap<T>::operator=(const UnorderedMap<T>& copy) {
 			continue;
 		}
 
-		_table[i] = new Node<T>(copy._table[i]->value, nullptr);
+		_table[i] = new Node<T>(copy._table[i]->key, copy._table[i]->value, nullptr);
 		Node<T>* ptr_table = _table[i];
 		Node<T>* ptr_copy = copy._table[i]->next;
 
@@ -130,20 +131,109 @@ UnorderedMap<T>& UnorderedMap<T>::operator=(const UnorderedMap<T>& copy) {
 template <typename T>
 void UnorderedMap<T>::print() {
 	for (size_t i = 0; i < _capacity; i++) {
-		Node<T> cur;
-		if (cur = _table[i]) {
-			cout << "[ key = " << cur->key << " ], < values = ";
-			while (cur) {
-				cout << cur->value << " ";
-				cur = cur->next;
-			}
-			cout << " >\n";
+		Node<T> cur = _table[i];
+		while (cur) {
+			cout << "[ key = " << cur->key << " ], < value = " << cur->value << " >\n";
+			cur = cur->next;
 		}
 	}
 }
 
 template <typename T>
 bool UnorderedMap<T>::contains(const T& value) {
-	
+	// Проход по всей таблице и поиск значения черех всю неё
+	for (size_t i = 0; i < _capacity; ++i) {
+		Node<T>* ptr = _table[i];
+		while (ptr) {
+			if (ptr->value == value) {
+				return true;
+			}
+			ptr = ptr->next;
+		}
+	}
+	return false;
+}
+
+template <typename T>
+int UnorderedMap<T>::count(int key) {
+	size_t pos = hash(key);
+	Node<T>* ptr = _table[pos];
+	int result = 0;
+	while (ptr) {
+		result++;
+		ptr = ptr->next;
+	}
+	return result;
+}
+
+template <typename T>
+T* UnorderedMap<T>::search(int key) {
+	size_t pos = hash(key);
+	Node<T>* ptr = _table[pos];
+	while (ptr) {
+		if (ptr->key == key && ptr->value == value) {
+			return ptr->value
+		}
+		ptr = ptr->next;
+	}
+	return nullptr;
+}
+
+template <typename T>
+bool UnorderedMap<T>::erase(int key) {
+	size_t pos = hash(key);
+	Node<T>* ptr = _table[pos];
+
+	// Отдельно рассмотрим случай, когда удаляемый элемент находится в голове списка
+	if (ptr->key == key) {
+		_table[pos] = ptr->next;
+		delete ptr;
+		ptr = nullptr;
+		return true;
+	}
+
+	// Если не в голове, тогда будем удалять элемент при помощи
+	// вспомогательного указателя ptr_prev на предыдущий элемент
+	Node<T>* ptr_prev = ptr;
+	ptr = ptr->next;
+	while (ptr) {
+		if (ptr->key == key) {
+			ptr_prev->next = ptr->next;
+			delete ptr;
+			ptr = nullptr;
+			return true;
+		}
+		ptr = ptr->next;
+	}
+	return false;
+}
+
+template <typename T>
+bool UnorderedMap<T>::insert(int key, const T& value) {
+	size_t pos = hash(key);
+	Node<T>* ptr = _table[pos];
+	// Проверим, есть ли уже такой элемент
+	while (ptr) {
+		if (ptr->key == key) {
+			// Если нашлась запись с этим ключом...
+			return false;
+		}
+	}
+	// Если не нашлось...
+	_table[pos] = new Node(key, value, _table[pos]);
+	return true;
+}
+
+template <typename T>
+void UnorderedMap<T>::insert_or_assign(int key, const T& value) {
+	size_t pos = hash(key);
+	Node<T>* ptr = _table[pos];
+	while (ptr) {
+		if (ptr->key == key) {
+			ptr->value = value;
+		}
+		ptr = ptr->next;
+	}
+	_table[pos] = new Node(key, value, _table[pos]);
 }
 #endif
